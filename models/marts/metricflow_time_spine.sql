8594255552aa21366846041be39459b39c7add30
manish shrestha <manish.shrestha+1781715866@dbtlabsworkshop.com>
2026-06-17 18:29:48 +0000

    semantic layer start session

---
+++ models/marts/metricflow_time_spine.sql
@@ -0,0 +1,23 @@
+{{
+    config(
+        materialized = 'table',
+    )
+}}
+
+with days as (
+    {{ dbt.date_spine(
+        'day',
+        "to_date('2000-01-01', 'YYYY-MM-DD')",
+        "to_date('2026-07-01', 'YYYY-MM-DD')"
+    ) }}
+),
+
+final as (
+    select cast(date_day as date) as date_day
+    from days
+)
+
+select *
+from final
+where date_day > dateadd(year, -4, current_timestamp())
+  and date_day < dateadd(day, 30, current_timestamp())
